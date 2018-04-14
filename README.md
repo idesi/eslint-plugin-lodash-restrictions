ec6eaa491d1ec2441d9278740a63165149425511
Watandeep Sekhon <watan.cp@gmail.com>
2018-04-14 16:02:52 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-lodash-restrictions
+
+Restricts and/or prevents usage of certain lodash functions
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-lodash-restrictions`:
+
+```
+$ npm install eslint-plugin-lodash-restrictions --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-lodash-restrictions` globally.
+
+## Usage
+
+Add `lodash-restrictions` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "lodash-restrictions"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "lodash-restrictions/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/no-lodash-isundefined.js
@@ -0,0 +1,64 @@
+/**
+ * @fileoverview Prevents usage of _.isUndefined method
+ * @author Watandeep Sekhon
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-lodash-isundefined"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+
+const errors = [{
+    messageId: 'invalid',
+    type: "MemberExpression"
+}];
+
+ruleTester.run("no-lodash-undefined", rule, {
+
+    valid: [
+      "valid = 5;",
+      "test2 = function(opts) { return _.forEach([1, 2], function(v){ return v + 1}) }",
+      "test = function() { if(test){ return; } }",
+      "obj = { method1 : function(){} }",
+      "test = function() { return test ? 1 : 2 }",
+      "test = true ? 1 : 2",
+    ],
+
+    invalid: [
+        {
+            code: "test = _.isUndefined(5)",
+            errors
+        },
+        {
+            code: "test2 = function(opt) { return _.isUndefined(opt) }",
+            errors
+        },
+        {
+            code: "test2 = function() { if(_.isUndefined(opt)) { return false; } }",
+            errors
+        },
+        {
+            code: "obj = { method1 : function(){ _.isUndefined(5) ? 1 : 2 } }",
+            errors
+        },
+        {
+            code: "test2 = function(val) { return _.isUndefined(val) ? 1 : 2 }",
+            errors
+        },
+        {
+            code: "test = _.isUndefined(5) ? 1 : 2",
+            errors
+        }
+    ]
+});
